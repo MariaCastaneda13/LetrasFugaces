8820faf0ad5bf996c5320767d7f53b51d5858658
Maria Castaneda <mariafcastanedaa@gmail.com>
2024-01-24 20:50:10 -0500

    Se ajusta el ItemDetailContainer agregandole const e import

--- src/assets/ItemListContainer/ItemListContainer.jsx
+++ src/assets/ItemListContainer/ItemListContainer.jsx
@@ -1,27 +1,27 @@
 
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import ItemList from "../ItemList/ItemList";
 import { getProductos, getProductosPorCategoria } from "../../asyncmock";
 import { useParams } from "react-router-dom";
 
 
-const ItemListContainer=()=> {
- const [productos, setProductos] = useState ([]);
- 
- const {idCategoria}=useParams();
- useEffect (() => {
-  const funcionProductos= idCategoria ? getProductosPorCategoria : getProductos;
-funcionProductos(idCategoria)
-.then (res=> setProductos (res))
-.catch(error => console.log(error))
+const ItemListContainer = () => {
+  const [productos, setProductos] = useState([]);
 
- },[idCategoria])
- return (
-  <div>
-    <h2> Mis productos </h2>
-    <ItemList productos={productos}/>
-  </div>
- )
+  const { idCategoria } = useParams();
+  useEffect(() => {
+    const funcionProductos = idCategoria ? getProductosPorCategoria : getProductos;
+    funcionProductos(idCategoria)
+      .then(res => setProductos(res))
+      .catch(error => console.log(error))
+
+  }, [idCategoria])
+  return (
+    <div>
+      <h2> Mis productos </h2>
+      <ItemList productos={productos} />
+    </div>
+  )
 }
 
 export default ItemListContainer
